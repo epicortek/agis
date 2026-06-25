c08822d6f62290839c4c3cadc35793a1c43e4505
Rizk Ayoub <rizk.b.ayoub@gmail.com>
2026-06-25 18:21:32 +0000

    Clean release artifacts from repository

--- .tmp/package-smoke/_sdk-import-check.mjs
+++
@@ -1,5 +0,0 @@
-import * as m from "/home/rizk/agis/.tmp/package-smoke/package/dist/index.js";
-const keys = Object.keys(m);
-if (keys.length === 0) { console.error('SDK_IMPORT_EMPTY'); process.exit(1); }
-console.log('SDK_IMPORT_OK (' + keys.length + ' exports)');
-console.log('Sample exports: ' + keys.slice(0, 5).join(', '));
